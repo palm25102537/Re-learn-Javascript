45c395914842a0aa2a3854aad9f49a9c2361c702
Wasin <xelticross@gmail.com>
2021-11-14 16:26:50 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let js='amazing'
+// if(js === 'amazing') alert('JS is Fun!!!')
+console.log(40+8+23-10)
+
+let firstName = "Jonas";
+firstName = "Matilda"
+//let firstName declaring by Camel Case
+//let first_name declaring by ...
+//declaring variable (variable calls firstName)
+//variable's name can't start with number
+//Can't name variable with reserve words in JS (function,new)
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
