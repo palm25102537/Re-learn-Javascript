754ca0711f67cae400666479cf37a1b97df9b8db
Wasin <xelticross@gmail.com>
2021-11-27 17:27:28 +0700

    if else statement

--- script.js
+++ script.js
@@ -38,7 +38,12 @@ const markBMI = markWeight/markHeight**2
 const johnBMI = johnWeight/johnHeight**2
 const markHigherBMI = markBMI > johnBMI
 
-console.log(`${markBMI}>${johnBMI}`,markHigherBMI)
+if(markHigherBMI){
+    console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})`)
+}else{
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})`)
+}
+// console.log(`${markBMI}>${johnBMI}`,markHigherBMI)
 
 markHeight = 1.88
 markWeight = 95
@@ -49,4 +54,43 @@ const markBMI2 = markWeight/markHeight**2
 const johnBMI2 = johnWeight/johnHeight**2
 const markHigherBMI2 = markBMI2 > johnBMI2
 
-console.log(`${markBMI2}>${johnBMI2}`,markHigherBMI2)
+// console.log(`${markBMI2}>${johnBMI2}`,markHigherBMI2)
+
+if(markHigherBMI2){
+    console.log(`Mark's BMI (${markBMI2}) is higher than John's (${johnBMI2})`)
+}else{
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI2}) `)
+}
+
+
+// function checkAge(age,name = 'this person'){
+//     const isOldEnough = age >= 18;
+//     if(isNaN(age)){
+//         console.log('Age must be integer')
+//         return;
+//     }else{
+//         if(isOldEnough){
+//             console.log(`${name} can start driving license 🚗`)
+//         }else{
+//             const yearsLeft = 18 - age
+//             console.log(`${name} is too young. Wait another ${yearsLeft} years :)`)
+//         }
+//     }
+// }
+
+// checkAge(19,'Tye')
+// checkAge(15,'Nune')
+
+// const birthYear = 2012;
+// let century;
+// if(birthYear <= 2000){
+//     century = 20
+// }else{
+//     century = 21
+// }
+
+// console.log(century)
+
+// if(true) do {}
+
+// variables that we declare in the block {} can't access from out side of the block {}
