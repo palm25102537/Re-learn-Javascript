64d53ee9333ba73293341ea4f188f95ec700f07f
Wasin <xelticross@gmail.com>
2022-04-03 09:35:31 +0700

    array

---
+++ array.js
@@ -0,0 +1,16 @@
+const friend1 = 'Micheal'
+const friend2 = 'Steven'
+const friend3 = "Peter"
+
+const friends = ['Michael','Steven','Peter'] //literal syntax
+console.log(friends)
+
+const years = new Array(1991,1984,2008,2020)
+
+console.log(friends[0]) // array are zero base
+
+console.log(friends.length) // actual amount elements in array
+
+friends[2] = 'Jay'
+
+console.log(friends)
