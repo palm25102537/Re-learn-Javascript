02f1b489f8a48f05f2b661dc686c32b1a60003d7
Wasin <xelticross@gmail.com>
2022-08-06 09:00:54 +0700

    code challenge

--- array.js
+++ array.js
@@ -80,60 +80,60 @@
 
 // testWord();
 
-const game = {
-  team1: "Bayern Munich",
-  team2: "Borrussia Dortmund",
-  players: [
-    [
-      "Neuer",
-      "Pavard",
-      "Martinez",
-      "Alaba",
-      "Davies",
-      "Kimmich",
-      "Goretzka",
-      "Coman",
-      "Muller",
-      "Gnarby",
-      "Lewandowski",
-    ],
-    [
-      "Burki",
-      "Schulz",
-      "Hummels",
-      "Akanji",
-      "Hakimi",
-      "Weigl",
-      "Witsel",
-      "Hazard",
-      "Brandt",
-      "Sancho",
-      "Gotze",
-    ],
-  ],
-  score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
-  date: "Nov 9th, 2037",
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: "Bayern Munich",
+//   team2: "Borrussia Dortmund",
+//   players: [
+//     [
+//       "Neuer",
+//       "Pavard",
+//       "Martinez",
+//       "Alaba",
+//       "Davies",
+//       "Kimmich",
+//       "Goretzka",
+//       "Coman",
+//       "Muller",
+//       "Gnarby",
+//       "Lewandowski",
+//     ],
+//     [
+//       "Burki",
+//       "Schulz",
+//       "Hummels",
+//       "Akanji",
+//       "Hakimi",
+//       "Weigl",
+//       "Witsel",
+//       "Hazard",
+//       "Brandt",
+//       "Sancho",
+//       "Gotze",
+//     ],
+//   ],
+//   score: "4:0",
+//   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+//   date: "Nov 9th, 2037",
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-const { scored, odds } = game;
+// const { scored, odds } = game;
 
-for ([idx, playerName] of scored.entries()) {
-  console.log(`Goal ${idx + 1}: ${playerName}`);
-}
+// for ([idx, playerName] of scored.entries()) {
+//   console.log(`Goal ${idx + 1}: ${playerName}`);
+// }
 
-let sum = 0;
-for ([key, value] of Object.entries(odds)) {
-  sum += value;
-  console.log(`Odd of ${game[key] ? "victory" + " " + game[key] : "draw"}`);
-}
-const avg = sum / Object.entries(odds).length;
-console.log(avg);
+// let sum = 0;
+// for ([key, value] of Object.entries(odds)) {
+//   sum += value;
+//   console.log(`Odd of ${game[key] ? "victory" + " " + game[key] : "draw"}`);
+// }
+// const avg = sum / Object.entries(odds).length;
+// console.log(avg);
 // const [player1, player2] = game.players;
 
 // console.log(player1, player2);
@@ -163,3 +163,76 @@ console.log(avg);
 
 // team1 < team2 && console.log("Team1 is more likely to win");
 // team1 > team2 && console.log("Team2 is more likely to win");
+
+// const rest = new Map();
+
+// rest.set("name", "TEST");
+// rest.set(1, "TEST");
+// rest.set(true, "TEST");
+// rest
+//   .set("testChain", "TEST")
+//   .set("chain", "test")
+//   .set(true, "Open")
+//   .set(false, "Close")
+//   .set("Open", 11)
+//   .set("Close", 23);
+
+// console.log(rest);
+
+// console.log(rest.get(true));
+
+// const time = 21;
+
+// console.log(rest.get(time > rest.get("Open") && time < rest.get("Close")));
+
+// const question = new Map([
+//   ["question", "What is the answer"],
+//   [1, "false"],
+//   [2, "false"],
+//   [3, "true"],
+//   ["correct", 3],
+//   [true, "Correct"],
+//   [false, "Try again"],
+// ]);
+
+// console.log(question.get("question"));
+
+// for ([key, value] of question) {
+//   if (typeof key === "number") {
+//     console.log(`Answer ${key} : ${value}`);
+//   }
+// }
+// const answer = +prompt("Your Answer");
+// console.log(typeof answer, answer);
+// console.log(question.get(answer === question.get("correct")));
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+const uniqueEvents = new Set(gameEvents.values());
+const events = [...uniqueEvents];
+console.log(events);
+gameEvents.delete(64);
+const avgEventHappened = 90 / gameEvents.size;
+const realAvgEventHappened = [...gameEvents.keys()].pop() / gameEvents.size;
+console.log(
+  `An event happened, on average , every ${avgEventHappened} minutes`
+);
+console.log(
+  `An event happened, on average , every ${realAvgEventHappened} minutes`
+);
+
+for (let [key, value] of gameEvents.entries()) {
+  console.log(`[${key <= 45 ? "FIRST" : "SECOND"} HALF] ${key} : ${value}`);
+}
